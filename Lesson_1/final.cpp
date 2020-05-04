c4df13fee43c4386dfefd2c380619040923d1d65
shivam <shivamsoni.soni3@gmail.com>
2020-05-05 00:17:30 +0530

    "some changes"

--- Lesson_1/final.cpp
+++ Lesson_1/final.cpp
@@ -11,27 +11,42 @@ using std::vector;
 
 enum class State {kEmpty, kObstacle};
 
-
+// TODO: Change the return type to use auto or
+// explicitly indicate vector<State>
+vector<State> ParseLine(string line) {
     istringstream sline(line);
     int n;
     char c;
+    // TODO: Change the variable type for row
+    // to be a vector<State>
     vector<State> row;
     while (sline >> n >> c && c == ',') {
-      if (n == 0) {
-        row.push_back(State::kEmpty);
-      } else {
-        row.push_back(State::kObstacle);
+      // TODO: Modify the line below to push_back
+      // a State::kEmpty if n is 0, and push_back
+      // a State::kObstacle otherwise.
+      
+      switch(n){
+    
+        case 0 : 	  row.push_back(State::kEmpty); break;
+        default :    row.push_back(State::kObstacle);
       }
     }
     return row;
 }
 
+// TODO: Modify the return type here as well. Just
+// as above, the board will contain State objects
+// instead of ints.
 vector<vector<State>> ReadBoardFile(string path) {
   ifstream myfile (path);
+  // TODO: Modify the board declarationto store 
+  // State objects instead of ints.
   vector<vector<State>> board{};
   if (myfile) {
     string line;
     while (getline(myfile, line)) {
+      // TODO: Modify the row type to use State
+      // objects instead of ints.
       vector<State> row = ParseLine(line);
       board.push_back(row);
     }
@@ -39,6 +54,8 @@ vector<vector<State>> ReadBoardFile(string path) {
   return board;
 }
 
+// TODO: Modify the function signature below to accept
+// cells with State type instead of int type.
 string CellString(State cell) {
   switch(cell) {
     case State::kObstacle: return "⛰️   ";
@@ -49,7 +66,9 @@ string CellString(State cell) {
 void PrintBoard(const vector<vector<State>> board) {
   for (int i = 0; i < board.size(); i++) {
     for (int j = 0; j < board[i].size(); j++) {
-      cout << CellString(board[i][j]);
+      // TODO: Modify the line below to call CellString
+      // on each element of the board before streaming to cout.
+      cout <<CellString( board[i][j]);
     }
     cout << "\n";
   }
